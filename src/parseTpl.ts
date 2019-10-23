7a274e64de1e4df8c5008ac35a897147117ff03d
lijie0981 <974277025@qq.com>
2019-10-23 16:16:24 +0800

    feature: add eslint

--- src/parseTpl.ts
+++ src/parseTpl.ts
@@ -6,14 +6,13 @@ import { parseCss, parseCssContent } from './parseCss';
 import { modifyUrl, getFileDataFromResourceMap } from './utils';
 import { parserOption } from '../global';
 import Debug from 'debug';
-let debug = Debug('inline:parseTpl');
-function parseTpl(file: any, options: parserOption) {
+const debug = Debug('inline:parseTpl');
+function parseTpl (file: any, options: parserOption) {
     debug('input file', file.path);
     let content = '';
     if (file.contents) {
         content = file.contents.toString();
-    }
-    else if (fs.existsSync(file.path)) {
+    } else if (fs.existsSync(file.path)) {
         content = fs.readFileSync(file.path, 'utf-8');
     }
 
@@ -31,95 +30,94 @@ function parseTpl(file: any, options: parserOption) {
     return content;
 }
 
-
 // 处理内联文件
-function parseTplContent(content: string, options: parserOption, file: any) {
+function parseTplContent (content: string, options: parserOption, file: any) {
     // 处理<link rel="stylesheet" type="text/css"> 这种内联的方式，如果在属性加上?__inline则表示需要内联。
     debug('parse tpl inline', file.path);
-    let linkReg = /<link.*href\s*=\s*('|")\/?(.+)\?(__lsInline|__inline)\1.*>/ig;
+    const linkReg = /<link.*href\s*=\s*('|")\/?(.+)\?(__lsInline|__inline)\1.*>/ig;
     content = content.replace(linkReg, function (all, quote, value, type) {
         let inlinecontent = '';
         if (type) {
             switch (type) {
-                case '__lsInline':
-                    debug('link lsInline', file.path);
-                    let lsPath = path.resolve(path.join(options.base, value));
-                    if (!fs.existsSync(lsPath)) {
-                        lsPath = path.resolve(path.join(path.dirname(file.path), value));
+            case '__lsInline':
+                debug('link lsInline', file.path);
+                let lsPath = path.resolve(path.join(options.base, value));
+                if (!fs.existsSync(lsPath)) {
+                    lsPath = path.resolve(path.join(path.dirname(file.path), value));
+                }
+                if (fs.existsSync(lsPath)) {
+                    const tmpPath = value.split('/');
+                    const name = tmpPath[tmpPath.length - 1].replace('.', '');
+                    const key = name.replace('.', '');
+                    const type = path.extname(lsPath).replace('.', '').replace(/less/, 'css');
+                    const content = parseCss({ path: lsPath }, options);
+                    const hash = getFileDataFromResourceMap(lsPath, options.sourceMapPath).md5;
+                    const srcPath = (options.staticDomain ? options.staticDomain : '') + '/se/' + value.replace(/(\.[a-zA-Z]+)$/, '') + '_' + hash + '.css';
+                    const captureStr = '{%capture name ="' + name + '"%}' + content + '{%/capture%}';
+                    const feLsInlineStr = '{%fe_ls_inline codeConf=["type"=>"' + type + '"' +
+                            ',"code"=>$smarty.capture.' + name +
+                            ',"key"=>"' + key + '"' +
+                            ',"path"=>"' + srcPath + '"' +
+                            ',"version"=>"' + hash + '"] lsControl=$lsControl%}';
+                    inlinecontent += captureStr + feLsInlineStr;
+                    if (!file.lsInline) {
+                        file.lsInline = [];
                     }
-                    if (fs.existsSync(lsPath)) {
-                        let tmpPath = value.split('/');
-                        let name = tmpPath[tmpPath.length - 1].replace('.', '');
-                        let key = name.replace('.', '');
-                        let type = path.extname(lsPath).replace('.', '').replace(/less/,'css');
-                        let content = parseCss({ path: lsPath }, options);
-                        let hash = getFileDataFromResourceMap(lsPath, options.sourceMapPath).md5;
-                        let srcPath = (options.staticDomain ? options.staticDomain : '') + '/se/' + value.replace(/(\.[a-zA-Z]+)$/,'') + '_' + hash + '.css';
-                        let captureStr = '{%capture name ="' + name + '"%}' + content + '{%/capture%}';
-                        let feLsInlineStr = '{%fe_ls_inline codeConf=["type"=>"' + type + '"'
-                            + ',"code"=>$smarty.capture.' + name
-                            + ',"key"=>"' + key + '"'
-                            + ',"path"=>"' + srcPath + '"'
-                            + ',"version"=>"' + hash + '"] lsControl=$lsControl%}';
-                        inlinecontent += captureStr + feLsInlineStr;
-                        if (!file.lsInline) {
-                            file.lsInline = [];
-                        }
-                        file.lsInline.push(lsPath);
-                    }
-                    break;
-                case '__inline':
-                    debug('link inline', file.path);
-                    let filePath = path.resolve(path.join(options.base, value));
-                    if (!fs.existsSync(filePath)) {
-                        filePath = path.resolve(path.join(path.dirname(file.path), value));
-                    }
-                    inlinecontent += parsefile(filePath, options, 'link', file);
-                    break;
+                    file.lsInline.push(lsPath);
+                }
+                break;
+            case '__inline':
+                debug('link inline', file.path);
+                let filePath = path.resolve(path.join(options.base, value));
+                if (!fs.existsSync(filePath)) {
+                    filePath = path.resolve(path.join(path.dirname(file.path), value));
+                }
+                inlinecontent += parsefile(filePath, options, 'link', file);
+                break;
             }
         }
         return inlinecontent;
     });
 
     // 处理<script src=""> 这种src方式。
-    let scriptReg = /<script.*src\s*=\s*('|")\/?(.+)\?(__lsInline|__inline)\1[^>]*>[\s\S]*?<\/script>/ig;
+    const scriptReg = /<script.*src\s*=\s*('|")\/?(.+)\?(__lsInline|__inline)\1[^>]*>[\s\S]*?<\/script>/ig;
 
     content = content.replace(scriptReg, function (all, quote, value, type) {
         let inlinecontent = '';
         if (type) {
             switch (type) {
-                case '__lsInline':
-                    debug('script lsInline', file.path);
-                    let lsPath = path.resolve(path.join(options.base, value));
-                    if (!fs.existsSync(lsPath)) {
-                        lsPath = path.resolve(path.join(path.dirname(file.path), value));
+            case '__lsInline':
+                debug('script lsInline', file.path);
+                let lsPath = path.resolve(path.join(options.base, value));
+                if (!fs.existsSync(lsPath)) {
+                    lsPath = path.resolve(path.join(path.dirname(file.path), value));
+                }
+                if (fs.existsSync(lsPath)) {
+                    const tmpPath = value.split('/');
+                    const name = tmpPath[tmpPath.length - 1].replace('.', '');
+                    const key = name.replace('.', '');
+                    const type = 'js';
+                    const content = parseJs({ path: lsPath }, options);
+                    const hash = getFileDataFromResourceMap(lsPath, options.sourceMapPath).md5;
+                    const srcPath = (options.staticDomain ? options.staticDomain : '') + '/se/' + value.replace(/(\.[a-zA-Z]+)$/, '') + '_' + hash + '.js';
+                    const captureStr = '{%capture name ="' + name + '"%}' + content + '{%/capture%}';
+                    const feLsInlineStr = '{%fe_ls_inline codeConf=["type"=>"' + type + '"' +
+                            ',"code"=>$smarty.capture.' + name +
+                            ',"key"=>"' + key + '"' +
+                            ',"path"=>"' + srcPath + '"' +
+                            ',"version"=>"' + hash + '"] lsControl=$lsControl%}';
+                    inlinecontent += captureStr + feLsInlineStr;
+                    if (!file.lsInline) {
+                        file.lsInline = [];
                     }
-                    if (fs.existsSync(lsPath)) {
-                        let tmpPath = value.split('/');
-                        let name = tmpPath[tmpPath.length - 1].replace('.', '');
-                        let key = name.replace('.', '');
-                        let type = 'js';
-                        let content = parseJs({ path: lsPath }, options);
-                        let hash = getFileDataFromResourceMap(lsPath, options.sourceMapPath).md5;
-                        let srcPath = (options.staticDomain ? options.staticDomain : '') + '/se/' + value.replace(/(\.[a-zA-Z]+)$/,'') + '_' + hash + '.js';
-                        let captureStr = '{%capture name ="' + name + '"%}' + content + '{%/capture%}';
-                        let feLsInlineStr = '{%fe_ls_inline codeConf=["type"=>"' + type + '"'
-                            + ',"code"=>$smarty.capture.' + name
-                            + ',"key"=>"' + key + '"'
-                            + ',"path"=>"' + srcPath + '"'
-                            + ',"version"=>"' + hash + '"] lsControl=$lsControl%}';
-                        inlinecontent += captureStr + feLsInlineStr;
-                        if (!file.lsInline) {
-                            file.lsInline = [];
-                        }
-                        file.lsInline.push(lsPath);
-                    }
-                    break;
-                case '__inline':
-                    debug('inline', file.path);
-                    let jsPath = path.resolve(path.join(options.base, value));
-                    inlinecontent += parsefile(jsPath, options, 'script', file);
-                    break;
+                    file.lsInline.push(lsPath);
+                }
+                break;
+            case '__inline':
+                debug('inline', file.path);
+                const jsPath = path.resolve(path.join(options.base, value));
+                inlinecontent += parsefile(jsPath, options, 'script', file);
+                break;
             }
         }
         return inlinecontent;
@@ -128,25 +126,25 @@ function parseTplContent(content: string, options: parserOption, file: any) {
     return content;
 }
 
-function parsefile(filePath: string, options: parserOption, type:string, file: any) {
+function parsefile (filePath: string, options: parserOption, type:string, file: any) {
     debug('parse file', filePath);
     let inlinecontent = '';
     if (fs.existsSync(filePath)) {
         // 判断inline的文件的后缀名，无后缀增加js后缀并inline
         switch (path.extname(filePath)) {
-            case '.tpl':
-                inlinecontent += parseTpl({ path: filePath }, options);
-                break;
-            case '.css':
-                inlinecontent += '<style type="text/css">\n' + parseCss({ path: filePath }, options) + '\n</style>';
-                break;
-            case '.less':
-                inlinecontent += '<style type="text/css">\n' + parseCss({ path: filePath }, options) + '\n</style>';
-                break;
-            case '.js':
-                let jsContent = parseJs({ path: filePath }, options);
-                //把文件内容放到script标签 中间
-                return type === 'script' ? '<script type="text/javascript">\n' + jsContent + '\n</script>' : jsContent;
+        case '.tpl':
+            inlinecontent += parseTpl({ path: filePath }, options);
+            break;
+        case '.css':
+            inlinecontent += '<style type="text/css">\n' + parseCss({ path: filePath }, options) + '\n</style>';
+            break;
+        case '.less':
+            inlinecontent += '<style type="text/css">\n' + parseCss({ path: filePath }, options) + '\n</style>';
+            break;
+        case '.js':
+            const jsContent = parseJs({ path: filePath }, options);
+            // 把文件内容放到script标签 中间
+            return type === 'script' ? '<script type="text/javascript">\n' + jsContent + '\n</script>' : jsContent;
         }
         if (!file.inline) {
             file.inline = [];
@@ -158,4 +156,4 @@ function parsefile(filePath: string, options: parserOption, type:string, file: a
     return inlinecontent;
 }
 
-export { parseTpl }
+export { parseTpl };
